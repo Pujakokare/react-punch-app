8e56e2c7e82b795cb0e8a2e7af5ae587d1919dd4
Pujakokare <puja.sonur7@gmail.com>
2025-10-31 16:33:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 10000;
 
 const connectToCouchbase = async () => {
   try {
