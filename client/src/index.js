d2fd44b612195fb306380be2b0ee1181120daec0
Pujakokare <puja.sonur7@gmail.com>
2025-11-01 15:31:42 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,26 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import './App.css';
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
-//   <MsalProvider instance={msalInstance}>
+// ReactDOM.render(
+//   <React.StrictMode>
 //     <App />
-//   </MsalProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
 // );
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
 
-ReactDOM.render(
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./App.css"; // ensures your styles are loaded globally
+
+// Get root DOM node
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+// Render your app
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
