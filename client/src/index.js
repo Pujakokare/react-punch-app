defed3f46d48808040463cfbdff5b41c843be07d
Pujakokare <puja.sonur7@gmail.com>
2025-11-02 23:35:55 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,26 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
-import "./App.css"; // ensures your styles are loaded globally
 
-// Get root DOM node
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-// Render your app
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./App";
+// import "./App.css"; // ensures your styles are loaded globally
+
+// // Get root DOM node
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+
+// // Render your app
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
